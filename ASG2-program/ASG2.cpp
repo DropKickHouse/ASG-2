53a42c8f554f7022000db8b94554f4ed7ad62b28
Daive <162766674+DropKickHouse@users.noreply.github.com>
2025-09-01 11:55:46 +0700

    Add files via upload

---
+++ ASG2-program/ASG2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+  cout << "Hello!\n";
+  cout << "My Name is Dave Nathaniel\n";
+  cout << "My Favorite Program is Python\n";
+  return 0;
+}

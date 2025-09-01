550170d7fc57a857c77bcc6214cf02cbaa28ee8c
Daive <162766674+DropKickHouse@users.noreply.github.com>
2025-09-01 12:25:23 +0700

    Delete ASG2-program directory

--- ASG2-program/ASG2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std; 
-
-int main() {
-  cout << "Hello!\n";
-  cout << "My Name is Dave Nathaniel\n";
-  cout << "My Favorite Program is Python\n";
-  return 0;
-}
